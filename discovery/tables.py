798b9d1cfabeb594d39056bf39805c84696e620a
hephestos <hephestos@example.com>
2016-05-19 10:24:55 -0500

    include additional tables and static files

---
+++ discovery/tables.py
@@ -0,0 +1,14 @@
+import django_tables2 as tables
+from discovery.models import Interface, Socket, Connection
+
+class ConversationsTable(tables.Table):
+        src_port = tables.Column(accessor='src_socket.port')
+        src_addr = tables.Column(accessor='src_socket.interface.address_ether')
+        dst_port = tables.Column(accessor='dst_socket.port')
+        dst_addr = tables.Column(accessor='dst_socket.interface.address_ether')
+
+        class Meta:
+                fields = ['src_addr','src_port','dst_addr','dst_port','tx_pkts','tx_bytes','rx_pkts','rx_bytes']
+                model = Connection
+                # add class="paleblue" to <table> tag
+                attrs = {"class": "paleblue"}
