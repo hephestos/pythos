fcdc53c1cc67c3e54382f70ded032f47bceab38d
hephestos <hephestos@example.com>
2016-06-29 05:56:58 -0500

    PEP8 compliance

--- firewall/models.py
+++ firewall/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 
+
 class Firewall(models.Model):
         name = models.TextField()
 
+
 class Rule(models.Model):
         firewall = models.ForeignKey('Firewall', related_name='rules')
-        name = model.TextField()
+        name = models.TextField()
         src = models.ForeignKey('architecture.NetObject', related_name='src_rules')
         dst = models.ForeignKey('architecture.NetObject', related_name='dst_rules')
         protocol = models.IntegerField(default=0)
